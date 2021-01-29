3e0ce3b7ef882653be12c6ac96096a5e4b9bcd4f
Kash <kashay@agyeman.me>
2021-01-28 19:15:36 -0700

    printhigh score functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -207,3 +207,32 @@ function checkForEnter(event) {
 }
 
 
+// function to display high scores 
+function printHighscores() {
+  //redirect to display screen
+  titleScreen.setAttribute("class", "hide");
+  scoreDisplay.setAttribute("class", "show");
+  highscoreScreen.setAttribute("class", "hide");
+  hideHighScoresLink.setAttribute("class", "hide");
+  hideTime.setAttribute("class", "hide");
+
+  // either get scores from localstorage or set to empty array
+  var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+  // sort highscores by score property in descending order
+  highscores.sort(function(a, b) {
+    return b.score - a.score;
+  });
+
+  highscores.forEach(function(score) {
+    // create li tag for each high score
+    var liTag = document.createElement("li");
+    liTag.textContent = score.initials + " - " + score.score;
+
+    // display on page
+    var olEl = document.getElementById("display-scores");
+    olEl.appendChild(liTag);
+  });
+}
+
+
