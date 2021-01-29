f36f81161e2f012fa93cc709ba9f6035df49dce6
Kash <kashay@agyeman.me>
2021-01-28 18:59:28 -0700

    added mock questions and global variables

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+/* GLOBAL VARIABLES */ 
+
+// Variables to keep track of quiz state..................................................................................................................
+
+// index's for question loop
+var currentQuestionIndex = 0;
+// var ref in timerCountdown event (start timer at 75s)
+var time = 75;
+var timer;
+
+
+
+
+// Variables for DOM references..........................................................................................................................
+
+var timerCountdown = document.querySelector("#time");
+var startQuizBtn = document.querySelector("#start-quiz-btn");
+var initialBtn = document.querySelector("#initials-submit-button");
+var titleScreen = document.querySelector("#start-quiz");
+var quizScreen = document.querySelector("#quiz-section");
+var initialsEl = document.querySelector("#initials");
+var feedbackKey = document.querySelector("#key");
+var answersEl = document.querySelector("#answers");
+var highscoreScreen = document.querySelector("#highscore")
+var scoreDisplay = document.querySelector("#highscore-display")
+var hideHighScoresLink = document.querySelector("#view-highscores")
+var hideTime = document.querySelector(".time")
