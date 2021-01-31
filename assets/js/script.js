6f4d9a251a39421795d967a80d2172fb25060947
bstanfield1992 <bstanfield1992@gmail.com>
2021-01-31 10:11:14 -0700

    fixed ntilde formatting

--- assets/js/script.js
+++ assets/js/script.js
@@ -83,8 +83,8 @@ fetch('https://opentdb.com/api.php?amount=10&type=multiple')
         var randomAnswers = randomizeAnswers(answersVar);
         console.log(randomAnswers);
         questions[i] = {
-          quizQuestion: questionsString.replace(/&quot;/g, '\"').replace(/&#039;/g, '\'').replace(/&eacute;/g, 'é').replace(/&uuml;/g, 'ü').replace(/&ouml;/g, 'ö').replace(/&amp;/g, '&').replace(/&iacute;/g, 'í').replace(/&Delta;/g, 'Δ').replace(/&aacute;/g, 'á').replace(/&oacute;/g, 'ó') ,
-          correct: questionResults[i].correct_answer.replace(/&quot;/g, '\"').replace(/&#039;/g, '\'').replace(/&eacute;/g, 'é').replace(/&uuml;/g, 'ü').replace(/&ouml;/g, 'ö').replace(/&amp;/g, '&').replace(/&iacute;/g, 'í').replace(/&Delta;/g, 'Δ').replace(/&aacute;/g, 'á').replace(/&oacute;/g, 'ó') ,
+          quizQuestion: questionsString.replace(/&quot;/g, '\"').replace(/&#039;/g, '\'').replace(/&eacute;/g, 'é').replace(/&uuml;/g, 'ü').replace(/&ouml;/g, 'ö').replace(/&amp;/g, '&').replace(/&iacute;/g, 'í').replace(/&Delta;/g, 'Δ').replace(/&aacute;/g, 'á').replace(/&oacute;/g, 'ó').replace(/&ntilde;/g, 'ñ') ,
+          correct: questionResults[i].correct_answer.replace(/&quot;/g, '\"').replace(/&#039;/g, '\'').replace(/&eacute;/g, 'é').replace(/&uuml;/g, 'ü').replace(/&ouml;/g, 'ö').replace(/&amp;/g, '&').replace(/&iacute;/g, 'í').replace(/&Delta;/g, 'Δ').replace(/&aacute;/g, 'á').replace(/&oacute;/g, 'ó').replace(/&ntilde;/g, 'ñ') ,
           answers: randomAnswers
         }
         console.log(typeof questionResults[i].correct_answer);
@@ -236,7 +236,7 @@ fetch('https://opentdb.com/api.php?amount=10&type=multiple')
     var random = [];
     for (var i = 0; i < 4; i++) {
       var randomNumber = Math.floor(Math.random() * answers.length);
-      random.push(answers[randomNumber].replace(/&quot;/g, '\"').replace(/&#039;/g, '\'').replace(/&eacute;/g, 'é').replace(/&uuml;/g, 'ü').replace(/&ouml;/g, 'ö').replace(/&amp;/g, '&').replace(/&iacute;/g, 'í').replace(/&Delta;/g, 'Δ').replace(/&aacute;/g, 'á').replace(/&oacute;/g, 'ó'));
+      random.push(answers[randomNumber].replace(/&quot;/g, '\"').replace(/&#039;/g, '\'').replace(/&eacute;/g, 'é').replace(/&uuml;/g, 'ü').replace(/&ouml;/g, 'ö').replace(/&amp;/g, '&').replace(/&iacute;/g, 'í').replace(/&Delta;/g, 'Δ').replace(/&aacute;/g, 'á').replace(/&oacute;/g, 'ó').replace(/&ntilde;/g, 'ñ'));
       answers.splice(randomNumber, 1);
     }
     return random;
