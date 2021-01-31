2cd1776fbda9f43e2f66c2edbde26b94790d5903
bstanfield1992 <bstanfield1992@gmail.com>
2021-01-31 09:45:21 -0700

    fixed more fomatting, iacute &amp and ouml

--- assets/js/script.js
+++ assets/js/script.js
@@ -83,8 +83,8 @@ fetch('https://opentdb.com/api.php?amount=10&type=multiple')
         var randomAnswers = randomizeAnswers(answersVar);
         console.log(randomAnswers);
         questions[i] = {
-          quizQuestion: questionsString.replace(/&quot;/g, '\"').replace(/&#039;/g, '\'').replace(/&eacute;/g, 'é').replace(/&uuml;/g, 'ü') ,
-          correct: questionResults[i].correct_answer.replace(/&quot;/g, '\"').replace(/&#039;/g, '\'').replace(/&eacute;/g, 'é').replace(/&uuml;/g, 'ü') ,
+          quizQuestion: questionsString.replace(/&quot;/g, '\"').replace(/&#039;/g, '\'').replace(/&eacute;/g, 'é').replace(/&uuml;/g, 'ü').replace(/&ouml;/g, 'ö').replace(/&amp;/g, '&').replace(/&iacute;/g, 'í') ,
+          correct: questionResults[i].correct_answer.replace(/&quot;/g, '\"').replace(/&#039;/g, '\'').replace(/&eacute;/g, 'é').replace(/&uuml;/g, 'ü').replace(/&ouml;/g, 'ö').replace(/&amp;/g, '&').replace(/&iacute;/g, 'í') ,
           answers: randomAnswers
         }
         console.log(typeof questionResults[i].correct_answer);
@@ -236,9 +236,9 @@ fetch('https://opentdb.com/api.php?amount=10&type=multiple')
     var random = [];
     for (var i = 0; i < 4; i++) {
       var randomNumber = Math.floor(Math.random() * answers.length);
-      random.push(answers[randomNumber].replace(/&quot;/g, '\"').replace(/&#039;/g, '\'').replace(/&eacute;/g, 'é').replace(/&uuml;/g, 'ü'));
+      random.push(answers[randomNumber].replace(/&quot;/g, '\"').replace(/&#039;/g, '\'').replace(/&eacute;/g, 'é').replace(/&uuml;/g, 'ü').replace(/&ouml;/g, 'ö').replace(/&amp;/g, '&').replace(/&iacute;/g, 'í'));
       answers.splice(randomNumber, 1);
     }
     return random;
   }
-  ü
+  
